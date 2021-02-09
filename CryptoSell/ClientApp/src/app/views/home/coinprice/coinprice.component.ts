1b94607a28284f75d642b4be7c92929aa48fa8b8
Danilo Markovic <danilom99@hotmail.co.uk>
2021-02-09 20:13:05 +0100

    .

--- CryptoSell/ClientApp/src/app/views/home/coinprice/coinprice.component.ts
+++ CryptoSell/ClientApp/src/app/views/home/coinprice/coinprice.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-coinprice',
   templateUrl: './coinprice.component.html',
   styleUrls: ['./coinprice.component.scss']
 })
-export class CoinpriceComponent implements OnInit {
-  displayedColumns:  string[] = ['Symbol', 'Name', 'Price'];
+export class CoinpriceComponent implements AfterViewInit {
+  displayedColumns:  string[] = ['symbol', 'name', 'marketPrice'];
+  httpClient: HttpClient;
+  public coins: Coin[] = [];
 
-  constructor() { }
+  dataSource = new MatTableDataSource<Coin>(this.coins);
 
-  ngOnInit() {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+ 
+  constructor(http: HttpClient) {
+    http.get<Coin[]>('https://localhost:5001/' + 'coin/getcoins').subscribe(result => {
+      this.dataSource = new MatTableDataSource<Coin>(result);
+      this.ngAfterViewInit();
+    }, error => console.error(error));
   }
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+}
+
+interface Coin {
+  Symbol: string;
+  Name: string;
+  MarketPrice: number;
 }
