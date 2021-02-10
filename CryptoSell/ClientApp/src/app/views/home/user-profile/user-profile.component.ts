f1daf92703e42dfa32ca230a4c740c4ce1dddada
Danilo Markovic <danilom99@hotmail.co.uk>
2021-02-10 01:13:59 +0100

    .

--- CryptoSell/ClientApp/src/app/views/home/user-profile/user-profile.component.ts
+++ CryptoSell/ClientApp/src/app/views/home/user-profile/user-profile.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatTab, MatTableDataSource } from '@angular/material';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -15,13 +16,14 @@ export class UserProfileComponent implements AfterViewInit {
   displayedColumns:  string[] = ['Coin', 'Kolicina', 'Cena', 'Tip', 'Obrisi'];
   displayedColumns2:  string[] = ['Coin', 'Kolicina', 'Cena', 'Oglasivac', 'Obustavi'];
   httpClient: HttpClient;
-  currentUser: User = null;
+  currentUser: any;
+  username: string;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(http: HttpClient) {
 
-    http.get<User>('https://localhost:5001/' + 'user/getuser?username=' + localStorage.getItem("username")).subscribe(result => {
+    http.get('https://localhost:5001/' + 'user/getuser?username=' + localStorage.getItem("username")).subscribe(result => {
       this.currentUser = result;
     }, error => console.error(error));
     this.httpClient = http; 
@@ -43,6 +45,26 @@ export class UserProfileComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
+  }
+
+  deleteActiveAd(active: any)
+  {
+    this.httpClient.delete('https://localhost:5001/' + 'ad/deletebyguid?uid='+ active)
+    .subscribe((s) => {
+      console.log(s);
+      alert("Uspesno ste obrisali aktivni oglas!");
+      location.reload();
+    }, error => alert(error));
+  }
+
+  deleteProcessingAd(add: any)
+  {
+    this.httpClient.delete('https://localhost:5001/' + 'ad/deletebyguid?uid='+ add)
+    .subscribe((s) => {
+      console.log(s);
+      alert("Uspesno ste obustavili oglas!");
+      location.reload();
+    }, error => alert(error));
   }
 }
 
--- CryptoSell/Controllers/AdController.cs
+++ CryptoSell/Controllers/AdController.cs
@@ -57,7 +57,7 @@ namespace CryptoSell.Controllers
         [HttpGet(nameof(GetUserActiveAds))]
         public async Task<IActionResult> GetUserActiveAds(string username)
         {
-            var activeAds = Collection.Find(x => x.AdStatus == Enums.AdStatus.Active && x.Advertiser.UserName == username).ToList();
+            var activeAds = Collection.Find(x => (x.AdStatus == Enums.AdStatus.Active || x.AdStatus == Enums.AdStatus.Processing || x.AdType == Enums.AdType.Buy || x.AdType == Enums.AdType.Sell) && x.Advertiser.UserName == username).ToList();
 
             if (activeAds == null)
                 return BadRequest();
@@ -68,7 +68,7 @@ namespace CryptoSell.Controllers
         [HttpGet(nameof(GetUserProcessingAds))]
         public async Task<IActionResult> GetUserProcessingAds(string username)
         {
-            var processingAds = Collection.Find(x => x.AdStatus == Enums.AdStatus.Processing && x.Advertiser.UserName == username).ToList();
+            var processingAds = Collection.Find(x => x.AdStatus == Enums.AdStatus.Processing && x.Customer.UserName == username).ToList();
 
             if (processingAds == null)
                 return BadRequest();
@@ -105,6 +105,5 @@ namespace CryptoSell.Controllers
         {
             Collection.DeleteOne<Ad>(a => a.AdUid == uid);
         }
-
     }
 }
